04079be9c8d723605ece6bb2ec5835315476c0c0
Isaque correa <isq2006@gmail.com>
2024-09-15 22:44:25 -0300

    Primeiro Commit

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+var agora = document.querySelector(".horario");
+var img_agora = document.querySelector(".img_horario")
+var hoje = new Date();
+var hora_hoje = hoje.getHours();
+console.log(img_agora)
+
+if (hora_hoje < 12){
+    agora.innerHTML= `<p>Agora são ${hora_hoje} Horas.</p>`
+    img_agora.innerHTML = `<img src ="./img/dia.png">`
+}else if (hora_hoje < 18){
+    agora.innerHTML= `<p>Agora são ${hora_hoje} Horas.</p>`
+    img_agora.innerHTML = `<img src="./img/tarde.png" alt="noite">`
+}else{
+    agora.innerHTML= `<p>Agora são ${hora_hoje} Horas.</p>`
+    img_agora.innerHTML = `<img src ="./img/estradanortuna.png">`
+}
